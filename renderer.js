1a19559821fce790664e81f07f3036b6a747ae66
Ryder <dev@ryderdamen.com>
2018-07-01 11:55:51 -0400

    Adding randomization technique and basic styling

--- renderer.js
+++ renderer.js
@@ -7,33 +7,57 @@ compile();
 function compile() {
     var elem = document.getElementById('main-input');
     elem.addEventListener('keypress', function(e){
-      if (e.keyCode == 13) {
+      if (e.keyCode == 13 && !event.shiftKey) {
+        e.preventDefault() // Prevents addition of extra line break
         deleteEverything()
       }
     });
 }
 
-function funDelete() {
-    // Set up randomization
-    var randcharacters = '@#$%^&*{}~ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%^&*!{}[]~'
-    randcharacters = randcharacters.split();
+// Recursively randomizes characters in a string
+function recursivelyRandomizeCharacters(string, positionInString, originalLength) {
 
-    // Grab the current sentence
-    var sentence = document.getElementById('main-input').value
+    // TODO: Preserve spaces
 
-    // Loop through the sentence characters
-    for (var i = 0; i < sentence.split().length; i++) {
-        // Pick a random number
-        randomNumber = Math.floor(Math.random() * randcharacters.length)  
+    var randcharacters = '@#$%^&*{}~AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz@#$%^&*!{}[]~'.split("")
+    let randomNumber = Math.floor(Math.random() * randcharacters.length) 
+    var splitSentence = string.split("")
 
-        // Replace the first 
-        randcharacters[randomNumber]
-        
+    // Assign a random character, and set the view and the string, unless it's a space
+    if (splitSentence[positionInString] !== " ") {
+        splitSentence[positionInString] = randcharacters[randomNumber]
+    }
+    let newSentence = splitSentence.join("")
+    document.getElementById('main-input').value = newSentence
+    string = newSentence
+
+    // Break recursion
+    if (positionInString == 0) {
+        recursivelyRandomizeCharactersCallback()
+        return
     }
 
+    // Else, call the next string in the set
+    setTimeout(()=>{
+        recursivelyRandomizeCharacters(string, positionInString - 1, originalLength)
+    }, parseInt( 400 / originalLength));
 
 }
 
+// Callback for recursivelyRandomizeCharacters
+function recursivelyRandomizeCharactersCallback() {
+    document.getElementById('main-input').value = ""
+}
+
+// Deletes with a randomization process
+function funDelete() {
+    // Grab the current sentence and split it
+    var sentence = document.getElementById('main-input').value
+    var sentenceLength = sentence.split("").length - 1
+    recursivelyRandomizeCharacters(sentence, sentenceLength, sentenceLength)
+}
+
 function deleteEverything() {
-    document.getElementById('main-input').value = '';
+    funDelete()
+    // document.getElementById('main-input').value = ""
 }
